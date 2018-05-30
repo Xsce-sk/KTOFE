061dc9a43bb0b391dfdd5f4a8bead321a7e50781
Ace <ace.lowder@gmail.com>
2018-05-30 13:12:12 -0700

    Added Main Menu

---
+++ ForestFire/Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    // Private Members
+    private GameObject playButton;
+
+	void Start ()
+    {
+        playButton = GameObject.Find("PlayButton");
+        playButton.GetComponent<Transform>().position = new Vector3(0f, 1f, VRBounds.bounds.width / 2.5f);
+        playButton.GetComponent<Transform>().localScale = new Vector3(VRBounds.bounds.length - 0.1f, 0.3f, 0.25f);
+    }
+
+	void Update ()
+    {
+		
+	}
+}
